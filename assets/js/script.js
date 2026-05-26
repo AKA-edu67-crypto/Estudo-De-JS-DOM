55b6d91e26512e4fe6246a6f987cbc5c89e6000b
Eduardo Silva <shitedu67@gmail.com>
2026-05-26 11:44:30 -0300

    Exrcicio De DOM e aplicação De CSS #1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,26 @@
-function clicou () {
-    console.log("clicou no botão")
-}
+let botao = document.querySelector("#meuBotao")
+let titulo = document.querySelector("#titulo")
+let lista = document.querySelector("ul")
 
-//listener é um escutado de evento (como o evento "click")
-//essa é outra forma de fazer eventos
-let botao = document.querySelector(".botao")
-botao.addEventListener("click", clicou)
+
+botao.addEventListener("click", () => {
+    titulo.innerHTML = "O DOM È SENSACIONAL!"
+
+
+    let novoItem = document.createElement("li")
+    novoItem.innerHTML = "Novo Item Adicionado!"
+    lista.append(novoItem)
+    titulo.style.color = "blue"
+    lista.style.backgroundColor = "gold"
+})
+
+
+/* Capturou os elementos com o querySelector.
+
+Criou um sensor de clique com o addEventListener.
+
+Alterou textos dinamicamente com o innerHTML.
+
+Criou elementos HTML do zero com o createElement e injetou na tela com o append.
+
+Controlou o CSS da página diretamente pelo JavaScript com o .style*/
